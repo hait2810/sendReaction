3f11112558e4f20c83914dec03bbde9c3472dc9e
hait2810 <ngvhai.dev@gmail.com>
2025-02-21 09:36:16 +0700

    update

--- src/pages/Monitor/Monitor.tsx
+++ src/pages/Monitor/Monitor.tsx
@@ -2,21 +2,28 @@ import { io, Socket } from "socket.io-client";
 
 interface ServerToClientEvents {
   hit: (bank: string) => void;
+  b52: (bank: string) => void;
+  sun: (bank: string) => void;
+  rik: (bank: string) => void;
 }
-
 
 const socket: Socket<ServerToClientEvents, object> = io(
   "https://linhtinh.vidieu.net/"
 );
 const Monitor = () => {
   socket.on("hit", (bank: string) => {
-   console.log(bank, "hihi");
+    console.log(bank, "hit");
   });
-  return (
-    <div>
-
-    </div>
-  );
+  socket.on("rik", (bank: string) => {
+    console.log(bank, "rik");
+  });
+  socket.on("sun", (bank: string) => {
+    console.log(bank, "sun");
+  });
+  socket.on("b52", (bank: string) => {
+    console.log(bank, "b52");
+  });
+  return <div></div>;
 };
 
 export default Monitor;
