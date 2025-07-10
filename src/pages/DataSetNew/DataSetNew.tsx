811eea80a8228a5805778e3b809c68070cdf7b47
hait2810 <ngvhai.dev@gmail.com>
2025-07-10 11:54:33 +0700

    update

--- src/pages/DataSetNew/DataSetNew.tsx
+++ src/pages/DataSetNew/DataSetNew.tsx
@@ -156,7 +156,7 @@ const DataSetNew = () => {
                       className={`w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors `}
                     />
                   </div>
-                  <div className="flex items-center gap-2">
+                  <div className="flex items-center gap-6">
                     <button
                       type="button"
                       onClick={() => handleColor("red")}
