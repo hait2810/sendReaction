e8795b977a4f363f3c35c36c8272e3c43faede0f
hait2810 <ngvhai.dev@gmail.com>
2025-07-12 00:18:39 +0700

    update

--- src/pages/DataSetNew/DataSetNew.tsx
+++ src/pages/DataSetNew/DataSetNew.tsx
@@ -7,6 +7,7 @@ interface FormData {
   full_text?: string;
   img_base64?: string;
   name?: string;
+  key?: string
   colors: string;
 }
 
@@ -139,6 +140,21 @@ const DataSetNew = () => {
                       type="text"
                       {...register("name")}
                       placeholder="Nhập tên mày vào đây"
+                      className={`w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors `}
+                    />
+                  </div>
+                    <div className="space-y-2">
+                    <label
+                      htmlFor="field1"
+                      className="block text-sm font-medium  text-black"
+                    >
+                      Key
+                    </label>
+                    <input
+                      id="field1"
+                      type="text"
+                      {...register("key")}
+                      placeholder="Key"
                       className={`w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors `}
                     />
                   </div>
